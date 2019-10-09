f3901c11c5e1aa70b9c1f5c9e74d6534bfe171c2
VlomAndrew <andrej_karpov_98@mail.ru>
2019-10-09 16:19:14 +0300

    first

---
+++ TestTask/Figures.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestTask
+{
+    abstract public class Figures
+    {
+        protected List<double> edge;
+        protected int edgeCount;
+        public abstract double Area();
+    }
+
+
+    public class Triangle: Figures
+    {
+        public Triangle(double A, double B, double C)
+        {
+            edgeCount = 3;
+            if (!(A + B > C && B + C > A && A + C > B))
+            {
+                throw new ArgumentException(string.Format("невозможно построить такой треугольник со сторонами {0},{1},{2}",A,B,C));
+            }
+            edge = new List<double>(){A,B,C};
+        }
+
+        public bool IsRectangle()
+        {
+            var maxEdge = edge.Max();
+            var index = edge.IndexOf(maxEdge);
+            var katets = new List<double>(edge);
+            katets.Remove(maxEdge);
+
+            return Math.Pow(katets[0], 2) * Math.Pow(katets[1], 2) == Math.Pow(maxEdge, 2);
+
+
+        }
+        public override double Area()
+        {
+            var p = (edge[0] + edge[1] + edge[2]) / 2;
+            var area = Math.Sqrt(p * (p - edge[0]) * (p - edge[1]) * (p - edge[2]));
+            return area;
+        }
+    }
+
+    public class Rectangle : Figures
+    {
+        public Rectangle(double A, double B, double C, double D)
+        {
+            if (A!=C && B!=D)
+                throw new ArgumentException(string.Format("невозможно построить квадрас с такими сторонами {0},{1},{2},{3}",A,B,C,D));
+            edgeCount = 4;
+            edge = new List<double>(){A,B,C,D};
+        }
+
+        public override double Area()
+        {
+            return edge[0] * edge[1];
+        }
+    }
+
+    public class Circle : Figures
+    {
+        public Circle(double radius)
+        {
+            edgeCount = 1;
+            edge = new List<double>{radius};
+        }
+
+        public override double Area()
+        {
+            return Math.PI * Math.Pow(edge[0], 2);
+        }
+    }
+}
---
+++ TestTask/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace TestTask
+{
+
+    sealed class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            try
+            {
+                Triangle tr1 = new Triangle(10.0, 5.0, 2.0);
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Triangle tr2 = new Triangle(11, 10, 12);
+
+            Rectangle sq1 = new Rectangle(10, 10, 10, 10), sq2 = new Rectangle(10, 25, 10, 25);
+
+            Console.WriteLine(
+                " Треугольник 2 прямоугольный - {0},Площадь второго треугольник = {1}",
+                tr2.IsRectangle(),  tr2.Area());
+            try
+            {
+                Rectangle rec1 = new Rectangle(12,13,14,15);
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Circle cr1 = new Circle(10);
+            Console.WriteLine("Площадь круга {0}",cr1.Area());
+        }
+    }
+}
